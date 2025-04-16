0d68f88a79fab4199dde20a3b2d5de8b978a4e6b
nailakeisha <110005668+nailakeisha@users.noreply.github.com>
2025-04-16 20:32:06 +0700

    Manager.java

--- Manager.java
+++ Manager.java
@@ -1,27 +1,28 @@
 public class Manager extends Pekerja {
-    private String departemen;            // Menyimpan nama departemen manager
+    private String departemen; // Departemen tempat manager bekerja
 
     public Manager(String departemen, double gaji, int tahun, int bulan, int tanggal, String nama, String nik, boolean jenisKelamin, boolean menikah) {
-        super(gaji, tahun, bulan, tanggal, nama, nik, jenisKelamin, menikah); // Memanggil konstruktor Pekerja
-        this.departemen = departemen;     // Menyimpan nama departemen
+        super(gaji, tahun, bulan, tanggal, nama, nik, jenisKelamin, menikah); // Panggil konstruktor Pekerja
+        this.departemen = departemen; // Set nama departemen
     }
 
     @Override
     public double getPendapatan() {
-        return super.getPendapatan() + (0.1 * gaji); // Pendapatan ditambah 10% dari gaji karena jabatan sebagai manager
+        return super.getPendapatan() + (0.1 * gaji); // Tambah 10% dari gaji karena jabatan Manager
     }
 
     @Override
     public String toString() {
         return "--------------------------" +
-               "\nnama\t\t: " + nama +                         // Menampilkan nama
-               "\nnik\t\t: " + nik +                           // Menampilkan NIK
-               "\njenis kelamin\t: " + (jenisKelamin ? "Laki-laki" : "Perempuan") +
-               "\npendapatan\t: " + getPendapatan() +          // Menampilkan pendapatan
-               "\ntahun masuk\t: " + tahunMasuk.getDayOfMonth() + "  " + tahunMasuk.getMonthValue() + "  " + tahunMasuk.getYear() +
-               "\njumlah anak\t: " + jumlahAnak +              // Menampilkan jumlah anak
-               "\ngaji\t\t: " + gaji +                          // Menampilkan gaji
-               "\ndepartemen\t: " + departemen +               // Menampilkan departemen
+               "\nnama\t\t: " + nama +                           // Nama
+               "\nnik\t\t: " + nik +                             // NIK
+               "\njenis kelamin\t: " + (jenisKelamin ? "Laki-laki" : "Perempuan") + // Jenis kelamin
+               "\npendapatan\t: " + getPendapatan() +            // Pendapatan total
+               "\ntahun masuk\t: " + tahunMasuk.getDayOfMonth() + "  " + tahunMasuk.getMonthValue() + "  " + tahunMasuk.getYear() + // Tanggal masuk
+               "\njumlah anak\t: " + jumlahAnak +                // Jumlah anak
+               "\ngaji\t\t: " + gaji +                            // Gaji pokok
+               "\ngaji total\t: " + getGajiTotal() +             // Gaji total (gaji + bonus)
+               "\ndepartemen\t: " + departemen +                 // Nama departemen
                "\n--------------------------";
-    }    
+    }
 }
